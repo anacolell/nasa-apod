c72fdbc412488cc32ca7cb75cb5047746466d640
ana colell <anacolell8@gmail.com>
2021-08-06 00:07:56 +0300

    Made navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
 
 export default function NavBar() {
+  const [show, setShow] = useState(true);
+  const controlNavbar = () => {
+    if (window.scrollY > 20) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && "nav-opaque"}`}>
       <div class="navbar-title">
         <img src={logo} alt="logo" className="logo" />
         <p>Astronomy Picture of the Day</p>
       </div>
       <div className="navbar-links">
-        <p className="navbar-link">See more pictures</p>
         <Link className="navbar-link" to="/">
           Home
         </Link>
